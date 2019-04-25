50569ea822de4cf74cf2cb044db5c2179cfdcebb
Cole Woulf <woulfc8602@my.uwstout.edu>
2019-04-25 15:02:24 -0500

    Just added a base to customer

--- DVDProject/src/CustomerType.java
+++ DVDProject/src/CustomerType.java
@@ -1,4 +1,31 @@
 
 public class CustomerType extends Person{
 
+	private int rentedDvds;
+
+	
+	public CustomerType(int rentedDvds) {
+		super();
+		this.rentedDvds = rentedDvds;
+	}
+
+	public CustomerType(String name, int accountNum, int rentedDvds) {
+		super(name, accountNum);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getRentedDvds() {
+		return rentedDvds;
+	}
+
+	public void setRentedDvds(int rentedDvds) {
+		this.rentedDvds = rentedDvds;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerType [rentedDvds=" + rentedDvds + "]";
+	}
+	
+	
 }
