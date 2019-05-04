52af7feb74d92000b69b90753e5234bfa512c7a5
Student <Student@C-1062E5F30A19>
2019-05-04 12:17:21 -0500

    update

--- DVDProject/src/CustomerType.java
+++ DVDProject/src/CustomerType.java
@@ -1,11 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-public class CustomerType extends Person{
+import java.util.Scanner;
+
+public class CustomerType extends Person {
 
 	private int rentedDvds;
 
-	
 	public CustomerType(int rentedDvds) {
 		super();
 		this.rentedDvds = rentedDvds;
@@ -13,7 +14,7 @@ public class CustomerType extends Person{
 
 	public CustomerType(String name, int accountNum, int rentedDvds) {
 		super(name, accountNum);
-		
+
 	}
 
 	public int getRentedDvds() {
@@ -24,22 +25,27 @@ public class CustomerType extends Person{
 		this.rentedDvds = rentedDvds;
 	}
 
-	
 	public int displayAccountnum() {
 		return getAccountNum();
 	}
-	
-	public static void CustomerSomething() throws FileNotFoundException{
-		
+
+	// reading customer file into arraylist
+	public static ArrayList<CustomerType> Customers() throws FileNotFoundException {
+		File fd = new File("Customers.txt");
+		Scanner Input = new Scanner(fd);
+		ArrayList<CustomerType> CustList = new ArrayList<CustomerType>();
+
+		return CustList;
+
 	}
-	
-	//Need method for Rent a DVD; that is, add the rented DVD to the list
-	//Need method for Return a DVD; that is, delete the rented DVD from the list
-	
+
+	// Need method for Rent a DVD; that is, add the rented DVD to the list
+	// Need method for Return a DVD; that is, delete the rented DVD from the list
+
 	@Override
 	public String toString() {
-		return "CustomerType ["+"Name= "+getName()+ " Account Number= "+getAccountNum()+" RentedDvds=" + rentedDvds + "]";
-	} 
-	
-	
+		return "CustomerType [" + "Name= " + getName() + " Account Number= " + getAccountNum() + " RentedDvds="
+				+ rentedDvds + "]";
+	}
+
 }
