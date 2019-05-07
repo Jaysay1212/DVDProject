ddb8ccf9a9f1c858bbff29a6cb5be43dc3836d06
Student <Student@144.13.180.127>
2019-05-07 10:57:27 -0500

    editt

--- DVDProject/src/CustomerType.java
+++ DVDProject/src/CustomerType.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
 
 public class CustomerType extends Person{
 
 	private int rentedDvds;
+	private ArrayList<DvdType> rented;
 
 	
-	public CustomerType(int rentedDvds) {
+	public CustomerType(int rentedDvds, ArrayList<DvdType> rented ) {
 		super();
 		this.rentedDvds = rentedDvds;
+		this.rented = rented;
 	}
 
 	public CustomerType(String name, int accountNum, int rentedDvds) {
@@ -27,17 +30,29 @@ public class CustomerType extends Person{
 		return getAccountNum();
 	}
 	
-	//Need method for Rent a DVD; that is, add the rented DVD to the list
+	public ArrayList<DvdType> getRented() {
+		
+		return rented;
+	}
+	
+	public void setRented(ArrayList<DvdType> rented) {
+		this.rented = rented;
+	}
 	
 	
 	//Need method for Return a DVD; that is, delete the rented DVD from the list
 	public void removeDVD(DvdType item) {
-		for(int i = 0; i < rented.size; ++i) {
-			if(rented.get(i).getDvd().getMovie_name() == item.getMovie_name()) {
+		for(int i = 0; i < rented.size(); ++i) {
+			//if(rented.get(i).getDvd().getMovie_name() == item.getMovie_name()) {
 				rented.remove(i);
 			}
 		}
 		
+	}
+	
+	//Need method for Rent a DVD; that is, add the rented DVD to the list
+	public void addDVD(DvdType item) {
+		rented.add(item);
 	}
 	
 	@Override
